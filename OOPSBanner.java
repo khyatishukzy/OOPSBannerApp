4de62fb0bd0ad224279ddb4fa68dc90e2917ca88
Khyati <ks7704@srmist.edu.in>
2026-02-18 10:56:14 +0530

    [khyatishukzy] Added: UC2 - Print OOPS Banner

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,5 +1,14 @@
 public class OOPSBanner {
     public static void main(String[] args) {
         System.out.println("Welcome to OOPSBanner application");
+        printOOPSBanner();
+    }
+    
+    public static void printOOPSBanner() {
+        System.out.println("O   O  O   O  PPP   SSS");
+        System.out.println("O   O  O   O  P  P S   ");
+        System.out.println("O   O  O   O  PPP   SSS");
+        System.out.println("O   O  O   O  P       S");
+        System.out.println(" OOO    OOO   P    SSS ");
     }
 }
